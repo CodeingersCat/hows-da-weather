95344abc91124e10376fe55ecfe36d0f11e0c5a8
Shrawan Baruah <rocketpilot763@gmail.com>
2021-07-21 20:24:58 +0530

    Fixed some bugs

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ blankPage.style.display = "flex";
 top_date.style.display = "none";
 top_loc.style.display = "none";
 
-window.onload = () => {
+const frontPage = () => {
   var options = {
     "method": "GET",
     "headers": {
@@ -52,8 +52,7 @@ window.onload = () => {
     .then(res => res.json()
     )
     .then(data => {
-      h2.innerText = "Articles for you"
-
+      h2.innerText = "Articles for you";
       article1.setAttribute("href", data.value[0].url);
       article1.innerHTML = data.value[0].title;     
       img1.setAttribute("src", data.value[0].image.url) 
@@ -73,9 +72,15 @@ window.onload = () => {
       blankPage.style.display = "none";
       h2.innerHTML = "ERROR :( \nIf you're reading this while using a working internet connection, please <a href='mailto:shrawansb2000@gmail.com'><u>let me know .</u></a>"
     });
+}
+
+window.onload = () => {
+  h2.innerText = "Loading...."
+  frontPage();
 };
 
 const putData = () => {
+  console.log("HEY")
   city = srch.value;
   var API =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
@@ -86,80 +91,95 @@ const putData = () => {
   fetch(API)
     .then((res) => res.json())
     .then((data) => {
-      h2.style.display = "none";
-      blankPage.style.display = "none";
-      top_date.style.display = "block";
-      top_loc.style.display = "block";
-      weatherSection.style.display = "flex";
-      var forecastAPI =
-        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-        data.coord.lat +
-        "&lon=" +
-        data.coord.lon +
-        "&exclude=current, minutely, hourly" +
-        "&appid=" +
-        key +
-        "&units=metric";
+      console.log(data)
+      if(data.cod == 429){
+        while(data.cod != 429)
+          putData();
+          h2.innerHTML = "Please wait awhile, the site is facing high traffic."
+      }
+      else if(data.cod == 200){
+          h2.style.display = "none";
+          blankPage.style.display = "none";
+          top_date.style.display = "block";
+          top_loc.style.display = "block";
+          weatherSection.style.display = "flex";
+          var forecastAPI =
+            "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            data.coord.lat +
+            "&lon=" +
+            data.coord.lon +
+            "&exclude=current, minutely, hourly" +
+            "&appid=" +
+            key +
+            "&units=metric";
 
-      var prefix = "wi wi-owm-";
-      var id = data.weather[0].id;
-      var date = new Date(data.dt * 1000);
-
-      if (data.dt > data.sys.sunset) prefix += "night";
-      else prefix += "day";
-
-      ic.setAttribute("class", prefix + "-" + id);
-
-      curtemp.innerHTML = Math.floor(data.main.temp) + " &deg;C";
-      humid.innerHTML = "Humidity: " + Math.floor(data.main.humidity) + " %";
-      feels.innerHTML =
-        "Feels like " + Math.floor(data.main.feels_like) + " &deg;C";
-      maxTd.innerHTML = "&#9650;" + Math.floor(data.main.temp_max) + " &deg;C";
-      minTd.innerHTML = "&#9660;" + Math.floor(data.main.temp_min) + " &deg;C";
-      desc.innerHTML = data.weather[0].main;
-      document.getElementsByClassName("loc")[0].innerHTML =
-        "&#x1f4cc;" + data.name + ", " + countries[data.sys.country];
-      document.getElementsByClassName("date")[0].innerHTML =
-        Date(date).split("G")[0];
-
-      fetch(forecastAPI)
-        .then((res) => res.json())
-        .then((data) => {
           var prefix = "wi wi-owm-";
+          var id = data.weather[0].id;
+          var date = new Date(data.dt * 1000);
 
-          if (data.daily[0].dt > data.daily[0].sunset) prefix += "night-";
-          else prefix += "day-";
-          
-          forecast.childNodes[11].style.marginLeft = "4%"
-          for (var i = 1; i < 8; i++) {
-            if (i == 3 ||
-              i == 7){
-                forecast.childNodes[i].style.marginLeft = "4%"
+          if (data.dt > data.sys.sunset) prefix += "night";
+          else prefix += "day";
+
+          ic.setAttribute("class", prefix + "-" + id);
+
+          curtemp.innerHTML = Math.floor(data.main.temp) + " &deg;C";
+          humid.innerHTML = "Humidity: " + Math.floor(data.main.humidity) + " %";
+          feels.innerHTML =
+            "Feels like " + Math.floor(data.main.feels_like) + " &deg;C";
+          maxTd.innerHTML = "&#9650;" + Math.floor(data.main.temp_max) + " &deg;C";
+          minTd.innerHTML = "&#9660;" + Math.floor(data.main.temp_min) + " &deg;C";
+          desc.innerHTML = data.weather[0].main;
+          document.getElementsByClassName("loc")[0].innerHTML =
+            "&#x1f4cc;" + data.name + ", " + countries[data.sys.country];
+          document.getElementsByClassName("date")[0].innerHTML =
+            Date(date).split("G")[0];
+
+          fetch(forecastAPI)
+            .then((res) => res.json())
+            .then((data) => {
+              var prefix = "wi wi-owm-";
+
+              if (data.daily[0].dt > data.daily[0].sunset) prefix += "night-";
+              else prefix += "day-";
+              
+              forecast.childNodes[11].style.marginLeft = "4%"
+              for (var i = 1; i < 8; i++) {
+                if (i == 3 ||
+                  i == 7){
+                    forecast.childNodes[i].style.marginLeft = "4%"
+                  }
+                var day = new Date(data.daily[i].dt * 1000)
+                  .toDateString()
+                  .slice(0, 3);
+                var exactDate = new Date(data.daily[i].dt * 1000)
+                  .toDateString()
+                  .slice(4, 11);
+                var id = data.daily[i].weather[0].id;
+                info[i - 1].childNodes[1].innerHTML =
+                  "&#9650;" + Math.floor(data.daily[i].temp.max) + " &deg;C";
+
+                info[i - 1].childNodes[3].innerHTML =
+                  "&#9660;" + Math.floor(data.daily[i].temp.min) + " &deg;C";
+
+                fwrap[i - 1].childNodes[3].innerHTML =
+                  data.daily[i].weather[0].description;
+                fic[i - 1].setAttribute("class", "f-icon " + prefix + id);
+
+                dateInfo[i - 1].childNodes[1].innerHTML = day;
+                dateInfo[i - 1].childNodes[3].innerHTML = exactDate;
               }
-            var day = new Date(data.daily[i].dt * 1000)
-              .toDateString()
-              .slice(0, 3);
-            var exactDate = new Date(data.daily[i].dt * 1000)
-              .toDateString()
-              .slice(4, 11);
-            var id = data.daily[i].weather[0].id;
-            info[i - 1].childNodes[1].innerHTML =
-              "&#9650;" + Math.floor(data.daily[i].temp.max) + " &deg;C";
-
-            info[i - 1].childNodes[3].innerHTML =
-              "&#9660;" + Math.floor(data.daily[i].temp.min) + " &deg;C";
-
-            fwrap[i - 1].childNodes[3].innerHTML =
-              data.daily[i].weather[0].description;
-            fic[i - 1].setAttribute("class", "f-icon " + prefix + id);
-
-            dateInfo[i - 1].childNodes[1].innerHTML = day;
-            dateInfo[i - 1].childNodes[3].innerHTML = exactDate;
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }else if(data.cod == 400 || data.cod == 404){
+          blankPage.style.display = "none";
+          top_date.style.display = "none";
+          top_loc.style.display = "none";
+          weatherSection.style.display = "none";
+          h2.style.display = "block";
+          h2.innerHTML = "No places found. Please check your input."
+        }
     })
     .catch((err) => {
       console.log(err)
@@ -184,4 +204,7 @@ logo.addEventListener("click", () => {
   weatherSection.style.display = "none";
   top_loc.style.display = "none";
   top_date.style.display = "none";
+  h2.style.display = "flex"; 
+  h2.innerText = "Articles for you";
+  frontPage();
 });
